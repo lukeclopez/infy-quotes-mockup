a06ebfa0239f496290e278fd924fc25114c6eddf
jmrada14 <jmrada14@tutanota.com>
2020-01-31 21:39:25 -0500

    axios

---
+++ src/utils/API.js
@@ -0,0 +1,25 @@
+import axios from "axios";
+
+export default {
+    getUsers: () => {
+        return axios.get("http://localhost:3000/users")
+    },
+    getUserById: (id) => {
+        return axios.get("http://localhost:3000/users/" + id)
+    },
+    getTransactions: () => {
+        return axios.get("http://localhost:3000/transactions")
+    },
+    getTransactionsById: (id) => {
+        return axios.get("http://localhost:3000/transactions/" + id)
+    },
+    postTransactions: (data) => {
+        return axios.post("http://localhost:3000/transactions", data)
+    },
+    updateTransaction : (id, data) => {
+        return axios.put("http://localhost:3000/transactions" + id, data)
+    },
+    deleteTransaction: (id) => {
+        return axios.delete("http://localhost:3000/transactions" + id)
+    }
+};
