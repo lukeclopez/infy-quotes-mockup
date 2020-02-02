30c280906058d35c431e18c1bf0b928165f6a23f
jmrada14 <jmrada14@tutanota.com>
2020-02-01 21:46:06 -0500

    managing state

--- src/components/CustomerDetail.js
+++ src/components/CustomerDetail.js
@@ -2,21 +2,28 @@ import React, {useState, useEffect} from "react";
 import {Form} from "react-bootstrap";
 
 const CustomerDetails = () => {
+    const [zip, setZip] = useState("");
+    const [dlId, setdlId] = useState("");
+    const [age, setage] = useState("");
+
     return (
         <div className="container">
             <h2 align="left">Auto Quote Registration</h2>
     <Form>
         <Form.Group controlId="exampleForm.ControlInput1">
             <Form.Label>Zip Code</Form.Label>
-            <Form.Control type="text" placeholder="Ex: 10030" />
+            <p>{zip}</p>
+            <Form.Control value={zip} onChange={e => setZip(e.target.value)} type="text" placeholder="Ex: 10030" />
         </Form.Group>
         <Form.Group controlId="exampleForm.ControlInput1">
             <Form.Label>DL ID</Form.Label>
-            <Form.Control type="text" placeholder="DL ID" />
+            <p>{dlId}</p>
+            <Form.Control value={dlId} onChange={e => setdlId(e.target.value)} type="text" placeholder="DL ID" />
         </Form.Group>
         <Form.Group controlId="exampleForm.ControlInput1">
             <Form.Label>Age when you got first license</Form.Label>
-            <Form.Control type="number" placeholder="Age" />
+            <p>{age}</p>
+            <Form.Control value={age} onChange={e => setage(e.target.value)} type="number" placeholder="Age" />
         </Form.Group>
     </Form>
         </div>
--- src/components/RecentTransactions.js
+++ src/components/RecentTransactions.js
@@ -1,10 +1,35 @@
 import React, { useState, useEffect } from "react";
-import {Table, Button} from "react-bootstrap";
+import API from "../utils/API";
+import { Table } from "react-bootstrap";
 
 const RecentTransactions = () => {
 
     const [data, setData] = useState([]);
 
+    useEffect(()=>{
+            API.getTransactions()
+                .then(res => setData(res.data))
+                .catch(err => console.log(err));
+        }
+        , []
+    );
+
+    const renderTable = () => {
+      if(data.length){
+          return data.map((i, index ) => {
+              const { age, email, id, manufacturer, model, zip } = i
+              return(
+                  <tr key={id}>
+                      <td>{zip}</td>
+                      <td>{age}</td>
+                      <td>{email}</td>
+                      <td>{model}</td>
+                      <td>{manufacturer}</td>
+                  </tr>
+              )
+          })
+      }
+    };
 
     return (
         <Table striped bordered hover style={{marginTop: "20px"}}>
@@ -15,11 +40,10 @@ const RecentTransactions = () => {
                 <th>Email</th>
                 <th>Model</th>
                 <th>Manufacturer</th>
-                <th>Action</th>
             </tr>
             </thead>
             <tbody>
-
+            {renderTable()}
             </tbody>
         </Table>
     )
